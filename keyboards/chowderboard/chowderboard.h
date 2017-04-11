368e11f76777fbadf2502608cf00be1944d21c08
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2017-04-11 14:15:45 -0500

    change keymap for chowderboard

--- keyboards/chowderboard/chowderboard.h
+++ keyboards/chowderboard/chowderboard.h
@@ -23,12 +23,15 @@
 // The first section contains all of the arguments
 // The second converts the arguments into a two-dimensional array
 #define KEYMAP( \
-    k00, k01, k02, \
-      k10,  k11   \
-) \
-{ \
-    { k00, k01,   k02 }, \
-    { k10, KC_NO, k11 }, \
+    K01, K02, K03, K04, K05, K06, K07, K08, K09, K10, K11, K12, K13, \
+    K14, K15, K16, K17, K18, K19, K20, K21, K22, K23, K24, K25, K26, \
+    K27, K28, K29, K30, K31, K32, K33, K34, K35, K36, K37, K38, \
+    K39, K40, K41, K42, K43, K44, K45, K46 \
+) { \
+    { K01,   K02,   K03,   K04,   K05,   K06,   K07,   K08,   K09,   K10,   K11,   K12,   K13   }, \
+    { K14,   K15,   K16,   K17,   K18,   K19,   K20,   K21,   K22,   K23,   K24,   K25,   K26   }, \
+    { K27,   KC_NO, K28,   K29,   K30,   K31,   K32,   K33,   K34,   K35,   K36,   K37,   K38   }, \
+    { KC_NO, KC_NO, K39,   K40,   K41,   KC_NO, K42,   K43,   KC_NO, K44,   K45,   K46,   KC_NO }  \
 }
 
 #endif
--- keyboards/chowderboard/keymaps/default/keymap.c
+++ keyboards/chowderboard/keymaps/default/keymap.c
@@ -19,19 +19,19 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = KEYMAP(
-  KC_TAB,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_MINS,    KC_BSPC,
-  MO(1),    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
+  KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_MINS,    KC_BSPC,
+  MO(2),    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
   KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
-  ____,     KC_LCTL,    MO(2), KC_LGUI,     KC_SPC, MO(2),  KC_RALT,    ____ ),
+  KC_LCTL,     KC_LALT,    KC_LGUI, MO(1),     KC_SPC, MO(1),  KC_RGUI,    MO(2) ),
 [1] = KEYMAP(
-  ____,     ____,   ____,   ____,   ____,   ____,   ____,   KC_P7,  KC_P8,      KC_P9,  KC_PPLS,    KC_PMNS,    ____,
-  ____,     ____,   ____,   ____,   ____,   KC_LBRC,KC_RBRC,KC_P4,  KC_P5,      KC_P6,  KC_PAST,    KC_PEQL,    ____,
-  ____,     ____,   ____,   ____,   ____,   ____,   KC_P0,  KC_P1,  KC_P2,      KC_P3,  KC_PSLS,    ____,
-  ____,     ____,   ____,   ____,   ____,   ____,   ____,   ____ ),
+  KC_TAB,   ____, KC_BTN2,   KC_MS_U,   KC_BTN1,   ____,   ____,   KC_7,  KC_8,      KC_9,  KC_PLUS,   KC_MINS,   ____,
+  ____,     ____, KC_MS_L,   KC_MS_D,   KC_MS_R,   KC_LBRC,KC_RBRC,KC_4,  KC_5,      KC_6,  KC_ASTR,   KC_EQL,    ____,
+  ____,     ____,   ____,   ____,   KC_BTN3,   ____,   ____,   KC_1,  KC_2,      KC_3,  ____,      ____,
+  ____,     ____,   ____,   ____,   KC_0,   ____,   ____,   ____ ),
 [2] = KEYMAP(
-  KC_GRV, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN, ____, ____,
-  ____, KC_ESC, KC_VOLD, KC_VOLU, KC_MUTE, ____, KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT,  KC_INS, KC_DEL,
-  ____, KC_MENU, KC_MPRV, KC_MPLY, KC_MNXT, KC_HOME, KC_END,    KC_PGUP,    KC_PGDN,    ____, KC_BSLS, ____,
+  KC_GRV,   KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN, ____, ____,
+  ____, KC_ESC, KC_VOLD, KC_VOLU, KC_MUTE, ____, KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT,  KC_INS, KC_DEL, ____,
+  ____, KC_MENU, KC_MPRV, KC_MPLY, KC_MNXT, KC_HOME, KC_END,    ____,    ____,    ____, KC_BSLS, ____,
   ____,     ____,   ____,   ____,   ____,   ____,   ____,   ____ ),
 };
 
