851c524eadac537e56859a2c4e4578f2948d9b79
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2017-04-13 18:37:07 -0500

    add workspace layer to keymap

--- keyboards/chowderboard/keymaps/default/keymap.c
+++ keyboards/chowderboard/keymaps/default/keymap.c
@@ -16,23 +16,32 @@
 #include "chowderboard.h"
 
 #define ____ KC_TRNS
+#define _BASEL 0
+#define _NUML 1
+#define _MOVEL 2
+#define _WSPACEL 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[0] = KEYMAP(
+[_BASEL] = KEYMAP(
   KC_ESC,   KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,       KC_O,   KC_P,       KC_MINS,    KC_BSPC,
-  MO(2),    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
+  OSL(2),    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,       KC_L,   KC_SCLN,    KC_QUOT,    KC_ENT,
   KC_LSFT,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,    KC_DOT, KC_SLSH,    KC_RSFT,
-  KC_LCTL,     KC_LALT,    KC_LGUI, MO(1),     KC_SPC, MO(1),  KC_RGUI,    MO(2) ),
-[1] = KEYMAP(
+  KC_LCTL,     KC_LALT,    KC_LGUI, OSL(1),     KC_SPC, OSL(3),  KC_RGUI,    OSL(2) ),
+[_NUML] = KEYMAP(
   KC_TAB,   ____, KC_BTN2,   KC_MS_U,   KC_BTN1,   ____,   ____,   KC_7,  KC_8,      KC_9,  KC_PLUS,   KC_MINS,   ____,
   ____,     ____, KC_MS_L,   KC_MS_D,   KC_MS_R,   KC_LBRC,KC_RBRC,KC_4,  KC_5,      KC_6,  KC_ASTR,   KC_EQL,    ____,
   ____,     ____,   ____,   ____,   KC_BTN3,   ____,   ____,   KC_1,  KC_2,      KC_3,  ____,      ____,
   ____,     ____,   ____,   ____,   KC_0,   ____,   ____,   ____ ),
-[2] = KEYMAP(
+[_MOVEL] = KEYMAP(
   KC_GRV,   KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC,    KC_AMPR,    KC_ASTR,    KC_LPRN,    KC_RPRN, ____, ____,
   ____, KC_ESC, KC_VOLD, KC_VOLU, KC_MUTE, ____, KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT,  KC_INS, KC_DEL, ____,
   ____, KC_MENU, KC_MPRV, KC_MPLY, KC_MNXT, KC_HOME, KC_END,    ____,    ____,    ____, KC_BSLS, ____,
   ____,     ____,   ____,   ____,   ____,   ____,   ____,   ____ ),
+[_WSPACEL] = KEYMAP(
+  ____,   ____, ____,   ____,   ____,   ____,   ____,   LGUI(KC_7),  LGUI(KC_8),      LGUI(KC_9),  ____,   ____,   ____,
+  ____,     ____, ____,   ____,   ____,   ____,____,LGUI(KC_4),  LGUI(KC_5),      LGUI(KC_6),  ____,   ____,    ____,
+  ____,     ____,   ____,   ____,   ____,   ____,   ____,   LGUI(KC_1),  LGUI(KC_2),      LGUI(KC_3),  ____,      ____,
+  ____,     ____,   ____,   ____,   LGUI(KC_0),   ____,   ____,   ____ ),
 };
 
 const uint16_t PROGMEM fn_actions[] = {
