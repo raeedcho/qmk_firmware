c8cd1b22508d63b7341a5129772f8ac7917c1eb0
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2023-10-19 16:54:06 -0400

    feat: add colemak layer

--- keyboards/minibrick/keymaps/minimal/keymap.c
+++ keyboards/minibrick/keymaps/minimal/keymap.c
@@ -8,9 +8,10 @@
 
 enum layer_names {
     _BASL,
+    _QWEL,
     _NUML,
     _NAVL,
-    _SYML,
+    _EXTL,
     _GAML,
 };
 
@@ -34,35 +35,41 @@ enum keycodes {
 #define LA_NAVL MO(_NAVL)
 #define TT_NUML TT(_NUML)
 #define TT_NAVL TT(_NAVL)
-#define TT_SYML TT(_SYML)
+#define TT_EXTL TT(_EXTL)
 #define TT_GAML TT(_GAML)
+#define TT_QWEL TT(_QWEL)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASL] = LAYOUT(
-        KC_TAB, KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   KC_MINS,TT_NAVL,
-        KC_LCTL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,KC_QUOT,TT_GAML,
-        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,KC_BSPC,
-                KC_LALT,KC_LGUI,LA_NUML,KC_LSFT,        KC_SPC, LA_NAVL, KC_ENT,TT_NUML ),
+        KC_TAB, KC_Q,   KC_W,   KC_F,   KC_P,   KC_B,   KC_J,   KC_L,   KC_U,   KC_Y,   KC_QUOT,KC_BSPC,TT_NAVL,
+        KC_LCTL,KC_A,   KC_R,   KC_S,   KC_T,   KC_G,   KC_M,   KC_N,   KC_E,   KC_I,   KC_O,   KC_QUOT,TT_GAML,
+        KC_LSFT,KC_Z,   KC_X,   KC_C,   KC_D,   KC_V,   KC_K,   KC_H,   KC_COMM,KC_DOT, KC_QUES,TT_QWEL,
+                KC_ESC, KC_LGUI,LA_NUML,KC_LSFT,        KC_SPC, LA_NAVL,KC_BSPC,TT_NUML ),
+    [_QWEL] = LAYOUT(
+        _______,KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   _______,_______,
+        _______,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,_______,_______,
+        _______,KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,_______,
+                _______,_______,_______,_______,        _______,_______,_______,_______ ),
     [_NUML] = LAYOUT(
-        KC_ESC, KC_F9,  KC_F10, KC_F11, KC_F12, _______,_______,KC_7,   KC_8,   KC_9,   KC_PLUS,KC_MINS,_______,
-        _______,KC_F5,  KC_F6,  KC_F7,  KC_F8,  _______,_______,KC_4,   KC_5,   KC_6,   KC_ASTR,KC_EQL, _______,
-        _______,KC_F1,  KC_F2,  KC_F3,  KC_F4,  _______,_______,KC_1,   KC_2,   KC_3,   KC_SLSH,_______,
+        _______,KC_GRV, KC_AT,  KC_LCBR,KC_RCBR,KC_PERC,KC_ASTR,KC_7,   KC_8,   KC_9,   KC_MINS,_______,_______,
+        _______,KC_CIRC,KC_HASH,KC_LPRN,KC_RPRN,KC_DLR, KC_UNDS,KC_4,   KC_5,   KC_6,   KC_PLUS,_______, _______,
+        _______,KC_SLSH,KC_LABK,KC_LBRC,KC_RBRC,KC_RABK,KC_AMPR,KC_1,   KC_2,   KC_3,   KC_EQL, _______,
                 _______,_______,_______,_______,        KC_0,   _______,_______,_______ ),
     [_NAVL] = LAYOUT(
-        _______,KC_ESC, KC_MUTE,KC_VOLD,KC_VOLU,KC_TAB, _______,KC_END, KC_HOME,KC_MCTL,_______,KC_BSPC,_______,
-        _______,OS_SHFT,OS_CTRL,OS_ALT, OS_CMD, _______,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,KC_INS, KC_DEL, _______,
+        _______,KC_TAB, KC_MUTE,KC_VOLD,KC_VOLU,_______, _______,KC_END, KC_HOME,KC_END, _______,_______,_______,
+        _______,OS_SHFT,OS_CTRL,OS_ALT, OS_CMD, _______,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT, KC_ESC,_______,_______,
         _______,_______,KC_MPRV,KC_MPLY,KC_MNXT,_______,_______,KC_ENT, _______,_______,_______,_______,
                 _______,_______,_______,_______,        _______,_______,_______,_______ ),
-    [_SYML] = LAYOUT(
-        _______,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC,KC_CIRC,KC_AMPR,_______,_______,_______,KC_UNDS,_______,
-        _______,_______,KC_LCBR,KC_LBRC,KC_LPRN,KC_TILD,KC_GRV, KC_RPRN,KC_RBRC,KC_RCBR,KC_PIPE,_______,_______,
-        _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,KC_BSLS,_______,
+    [_EXTL] = LAYOUT(
+        _______,_______,_______,_______,_______,_______,_______,KC_F7,  KC_F8,  KC_F9,  KC_F10, _______,_______,
+        _______,_______,_______,_______,_______,_______,_______,KC_F4,  KC_F5,  KC_F6,  KC_F11, _______,_______,
+        _______,_______,_______,_______,_______,_______,_______,KC_F1,  KC_F2,  KC_F3,  KC_F12, _______,
                 _______,_______,_______,_______,        _______,_______,_______,_______ ),
     [_GAML] = LAYOUT(
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
         _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,
-                KC_ESC, _______,_______,KC_SPC,         _______,_______,_______,_______ ),
+                _______,_______,_______,KC_SPC,         _______,_______,_______,_______ ),
 };
 
 bool is_oneshot_cancel_key(uint16_t keycode) {
@@ -129,5 +136,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    return update_tri_layer_state(state, _NAVL, _NUML, _SYML);
+    return update_tri_layer_state(state, _NAVL, _NUML, _EXTL);
 }
+
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+const key_override_t question_override = ko_make_basic(MOD_MASK_SHIFT, KC_QUES, KC_EXLM);
+const key_override_t slash_override = ko_make_basic(MOD_MASK_SHIFT, KC_SLSH, KC_BSLS);
+const key_override_t amps_override = ko_make_basic(MOD_MASK_SHIFT, KC_AMPR, KC_PIPE);
+const key_override_t dot_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, KC_COLN);
+const key_override_t comma_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMMA, KC_SCLN);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+    &delete_key_override,
+    &question_override,
+    &slash_override,
+    &amps_override,
+    &dot_override,
+    &comma_override,
+    NULL // Null terminate the array of overrides!
+};
--- keyboards/minibrick/rules.mk
+++ keyboards/minibrick/rules.mk
@@ -1,3 +1,4 @@
 CAPS_WORD_ENABLE = yes
 SRC += oneshot.c
 SRC += swapper.c
+KEY_OVERRIDE_ENABLE = yes
